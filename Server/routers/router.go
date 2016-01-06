b62f8a653ed8f9c79b96d940e56bc020197224c4
archer <1246506786@qq.com>
2016-01-06 15:29:21 +0800

    bm book router

--- Server/routers/router.go
+++ Server/routers/router.go
@@ -7,4 +7,16 @@ import (
 
 func init() {
     beego.Router("/", &controllers.MainController{})
+
+		// BookController
+		beego.Router("/book", &controllers.BookController{}, "Put: CreateBook")
+		beego.Router("/book", &controllers.BookController{}, "Get: ReadBook")
+		beego.Router("/book", &controllers.BookController{}, "Post: UpdateBook")
+		beego.Router("/book", &controllers.BookController{}, "Delete: DeleteBook")
+
+		beego.Router("/books", &controllers.BookController{}, "Put: CreateBooks")
+		beego.Router("/books", &controllers.BookController{}, "Get: ReadBooks")
+		beego.Router("/books", &controllers.BookController{}, "Post: UpdateBooks")
+		beego.Router("/books", &controllers.BookController{}, "Delete: DeleteBooks")
+
 }
